d5d7f5f4ab88d01a742f4c61678b50e1de61b174
kjh910 <god12nom@gmail.com>
2022-04-20 12:58:45 +0900

    make templates

--- frontend/app/pages/index.tsx
+++ frontend/app/pages/index.tsx
@@ -1,56 +1,42 @@
-import type { NextPage } from 'next'
+import type { NextPage } from "next";
+import FloatingButton from "../components/floating-button";
+import Item from "../components/item";
+import Layout from "../components/layout";
 
 const Home: NextPage = () => {
   return (
-    <div className="bg-slate-400 py-20 px-10 grid gap-10">
-      <div className="bg-white p-5 rounded-3xl shadow-2xl">
-        <span className="font-semibold text-3xl">Select Item</span>
-        <div className="flex justify-between my-2">
-          <span className="text-gray-500">Grey Chair</span>
-          <span className="font-semibold">$19</span>
-        </div>
-
-        <div className="flex justify-between">
-          <span className="text-gray-500">Grey Chair</span>
-          <span className="font-semibold">$19</span>
-        </div>
-
-        <div className="flex justify-between mt-2 pt-2 border-t-2 border-dashed">
-          <span>Total</span>
-          <span className="font-semibold">$10</span>
-        </div>
-        
-        <div className="mt-5 bg-blue-500 text-white p-3 text-center rounded-xl w-2/4 mx-auto">Checkout</div>
-        
-        
+    <Layout title="홈" hasTabBar>
+      <div className="flex flex-col space-y-5 divide-y">
+        {[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map((_, i) => (
+          <Item
+            id={i}
+            key={i}
+            title="iPhone 14"
+            price={99}
+            comments={1}
+            hearts={1}
+          />
+        ))}
+        <FloatingButton href="/items/upload">
+          <svg
+            className="h-6 w-6"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            aria-hidden="true"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+            />
+          </svg>
+        </FloatingButton>
       </div>
+    </Layout>
+  );
+};
 
-      <div className="bg-white overflow-hidden rounded-3xl shadow-2xl">
-        <div>
-          <span className="bg-blue-500 p-6">Profile</span>
-        </div>
-        <div>
-          <div>
-            <div>
-              <span>Order</span>
-              <span>340</span>
-            </div>
-            <div />
-            <div>
-              <span>Spent</span>
-              <span>$2,310</span>
-            </div>
-            <div>
-              <span>Tony Molloy</span>
-              <span>New York, USA</span>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="bg-white p-10 rounded-3xl shadow-2xl"></div>
-      <div className="bg-white p-10 rounded-3xl shadow-2xl"></div>
-    </div>
-  )
-}
-
-export default Home
+export default Home;
